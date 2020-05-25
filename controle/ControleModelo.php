b2c073d61a9650c06f46e22f5e2d8fe4b6fa8df5
Luan Ribeiro <luan.ribeiro.dev@gmail.com>
2020-05-25 11:25:48 -0300

    commit

--- controle/ControleModelo.php
+++ controle/ControleModelo.php
@@ -20,16 +20,95 @@ class ControleModelo
 	{
 		$class = "<?php" .
 			"\n" .
-			"class ".$json_object['nome']." extends QueryBuilder{\n";
-		
-		ControleModelo::getAtributos($json_object, $class);
+			"class " . $json_object['nome'] . " extends QueryBuilder{\n";
 
-		$class.="\n}";
+		ControleModelo::checkAtributos($json_object, $class);
+		ControleModelo::getAtributos($json_object, $class);
+		ControleModelo::getConstrutor($json_object, $class);
+
+		$class .= "\n}";
 		echo $class;
+	}
+
+	private static function checkAtributos(array &$json_object)
+	{
+		$is_data_cadastro = false;
+		foreach ($json_object['atributos'] as $atributo) {
+			if ($atributo['nome'] == "data_cadastro") {
+				$is_data_cadastro = true;
+				break;
+			}
+		}
+
+		if(!$is_data_cadastro){
+			array_push($json_object['atributos'], [
+				"nome" => "data_cadastro",
+				"tipo" => "DateTime"
+			]);
+		}
 	}
 
 	private static function getAtributos(array $json_object, string &$class)
 	{
+		$class .= "\n";
+		foreach ($json_object['atributos'] as $atributo) {
+			$class .= "	/**\n";
+			$class .= "	 * @var " . $atributo['tipo'] . "\n";
+			$class .= "	 */\n";
+			$class .= "	private $" . $atributo['nome'] . ";\n\n";
+		}
+
+		$class .= "\n";
+	}
+
+	private static function getConstrutor(array $json_object, string &$class)
+	{
+		$class .= "\n";
+		$class .= "	public function __construct(";
+
+		// Argumentos Start
+		foreach ($json_object['atributos'] as $atributo) {
+			$class .= "$" . $atributo['nome'] . " = ";
+			if (isset($atributo['default']))
+				$class .= $atributo['default'];
+			else
+				$class .= "null";
+			$class .= ", ";
+		}
+
+		if (count($json_object['atributos']) > 0) {
+			$class = substr($class, 0, strlen($class) - 2);
+		}
+		// Argumentos End
+
+		$class .= ")\n";
+		$class .= "	{\n";
 		
+		// Parent Constructor Start
+		$class .= "		parent::__construct('" . $json_object['nome_tabela'] . "', [";
+
+		foreach (array_filter($json_object['atributos'], function ($atributo) {
+			return $atributo['nome'] != "deletado";
+		})
+			as
+			$atributo) {
+			$class.="'".$atributo['nome']."', ";
+		}
+
+		$class = substr($class, 0, strlen($class)-2);
+		$class.="]);\n";
+		// Parent Constructor END
+
+		// This Reference Start
+		$class .= "\n";
+		foreach($json_object['atributos'] as $atributo){
+			$class .= '		$this->'.$atributo['nome']." = ". '$'.$atributo['nome'].";\n";
+		}
+		// This Reference END
+
+		$class .= "	}\n";
+
+
+		$class .= "\n";
 	}
 }
