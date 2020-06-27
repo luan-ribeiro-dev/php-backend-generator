6e4a77dde292e3716f216a66719cdc2593e0287c
Luan Ribeiro <luan.ribeiro.dev@gmail.com>
2020-06-26 21:09:19 -0300

    changes

--- controle/Controle.php
+++ controle/Controle.php
@@ -112,7 +112,7 @@ class Controle
 
   public static function copyViewDefault()
   {
-    Controle::copy_dir(ROOT_LAYOUT_VIEW, VIEW_LAYOUT_TEMPLATE);
+    Controle::copy_dir(ROOT_LAYOUT_VIEW, VIEW_LAYOUT);
   }
 
   public static function copyExceptionDefaultClasses()
